13c2c6aa1f590489b808db560a88f483c03cee92
xAbdx <67425264+xAbdx@users.noreply.github.com>
2020-10-12 18:15:07 +0300

    edit ListMaxService

--- src/main/java/org/testteam/services/ListMaxService.java
+++ src/main/java/org/testteam/services/ListMaxService.java
@@ -1,4 +1,10 @@
 package org.testteam.services;
+import java.util.Arrays;  //you have to import java util arrays
 
 public class ListMaxService {
+    public static void main(String[] args){
+        int arr[] = {10, 324, 45, 90, 23};
+        int max = Arrays.stream(arr).max().getAsInt();
+        System.out.println("Largest in given array is " + max);
+    }
 }
